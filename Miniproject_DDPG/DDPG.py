fa1a20a632d4e83efe4a4ee948f527ae0f106f0d
ChangChunTzu <1037720176jessica@gmail.com>
2023-04-27 19:36:08 +0200

    first commit

---
+++ Miniproject_DDPG/DDPG.py
@@ -0,0 +1,109 @@
+import gym
+from helpers import NormalizedEnv, RandomAgent
+import numpy as np
+import matplotlib.pyplot as plt
+
+class HeuristicPendulumAgent():
+    def __init__(self, env):
+        self.state_size = env.observation_space.shape[0]
+        self.action_size = env.action_space.shape[0]
+        self.agent_name='HeuristicPendulumAgent'
+
+    def get_agent_name(self):
+        return self.agent_name
+
+    def compute_action(self, state,fix_torque=0.1):
+         # When the pendulum is in the lower half of the domain (x<0)
+         if state[0]<0:
+             # applies a fixed torque in the same direction as the pendulum’s angular velocity
+             return fix_torque*np.sign(state[2])
+         # When the pendulum is in the higher half of the domain (x>0)
+         else:
+             # applies a fixed torque in the  opposite direction as the pendulum’s angular velocity
+             return -1*fix_torque*np.sign(state[2])
+
+def DoState(env,agent,observation,total_reward_per_episode,agent_name='RandomAgent',fix_torque=0.1):
+    if agent_name=='HeuristicPendulumAgent':
+        action = agent.compute_action(observation,fix_torque)
+    else:
+        action = agent.compute_action(observation)
+    # print('action:',action)
+    normalized_env = NormalizedEnv(env)
+    normalized_action = normalized_env.action(action)
+    # print('normalized_action:',normalized_action)
+    observation, reward, terminated, truncated, info = env.step(normalized_action)
+    # print('observation:',observation)
+    # print('reward:',reward)
+    # print('terminated:',terminated)
+    total_reward_per_episode += reward
+    # print('total_reward:',total_reward)
+    if terminated or truncated:
+        observation, info = env.reset()
+
+    return observation, info, total_reward_per_episode
+
+def DoEpisode(env,agent,agent_name='RandomAgent',fix_torque=0.1,num_state=200) :
+    observation, info = env.reset()
+    total_reward_per_episode=0
+    for _ in range(num_state):
+        observation, info, total_reward_per_episode=DoState(env,agent,observation,total_reward_per_episode,agent_name,fix_torque)
+    return total_reward_per_episode
+
+
+
+env = gym.make("Pendulum-v1")
+num_episode=10
+
+# draw plot to show the impact of different amplitude of the fixed torque have on the reward
+list_last_average_reward=[]
+list_fix_torque=np.linspace(0.1, 2.0, num=20)
+print(list_fix_torque)
+total_episodes_reward_HeuristicAgent = 0
+for fix_torque in list_fix_torque:
+    for episode in range(num_episode):
+        agent = HeuristicPendulumAgent(env)
+        total_reward_per_episode=DoEpisode(env,agent,agent.get_agent_name(),fix_torque)
+        total_episodes_reward_HeuristicAgent += total_reward_per_episode
+        print('total_reward:', total_episodes_reward_HeuristicAgent)
+        average_reward = total_episodes_reward_HeuristicAgent / (episode + 1)
+        print('average_reward:', average_reward)
+    list_last_average_reward.append(average_reward)
+plt.plot(list_fix_torque,list_last_average_reward,'-o')
+plt.xlabel("Fix Torque")
+plt.ylabel("Average Reward")
+plt.title('Average Reward with differnt Fixed Torque (HeuristicPendulumAgent)')
+plt.xticks(list_fix_torque)
+plt.show()
+
+list_average_reward_HeuristicAgent=[]
+total_episodes_reward_HeuristicAgent = 0
+for episode in range(num_episode):
+    agent = HeuristicPendulumAgent(env)
+    total_reward_per_episode=DoEpisode(env, agent,agent.get_agent_name())
+    total_episodes_reward_HeuristicAgent += total_reward_per_episode
+    print('total_reward:', total_episodes_reward_HeuristicAgent)
+    average_reward = total_episodes_reward_HeuristicAgent / (episode + 1)
+    print('average_reward:', average_reward)
+    list_average_reward_HeuristicAgent.append(average_reward)
+
+list_average_reward_RandomAgent=[]
+total_episodes_reward_RandomAgent = 0
+for episode in range(num_episode):
+    agent = RandomAgent(env)
+    total_reward_per_episode=DoEpisode(env, agent)
+    total_episodes_reward_RandomAgent += total_reward_per_episode
+    print('total_reward:', total_episodes_reward_RandomAgent)
+    average_reward = total_episodes_reward_RandomAgent / (episode + 1)
+    print('average_reward:', average_reward)
+    list_average_reward_RandomAgent.append(average_reward)
+
+list_number_episodes = np.linspace(1,10,num=10)
+
+plt.plot(list_number_episodes,list_average_reward_HeuristicAgent,label= 'Average cumulative reward obtained by the heuristic policy')
+plt.plot(list_number_episodes,list_average_reward_RandomAgent,label= 'Average cumulative reward obtained by the random agent')
+plt.xlabel('Number of episodes')
+plt.ylabel('Average cumulative reward')
+plt.legend()
+plt.title('Compare the average cumulative reward obtained by the heuristic policy with the one obtained by the random agent')
+plt.show()
+env.close()
